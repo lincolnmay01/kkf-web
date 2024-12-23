35033dd760c8e25dbe51182c71aa7c612d346426
lincolnmay01 <ljmay002@louisville.edu>
2024-12-23 14:50:40 -0500

    added main src

---
+++ script.js
@@ -0,0 +1,9 @@
+/*links to social media*/
+document.getElementById('emailButton').addEventListener('click', function() {
+    window.location.href = 'mailto:kristinakf04@gmail.com';
+  });
+
+  document.getElementById("linkedinbutton").addEventListener("click", function() {
+    var linkedInUrl = "https://www.linkedin.com/in/lincoln-may";   
+    window.open(linkedInUrl, "_blank");
+});
