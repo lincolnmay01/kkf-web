466dfcca1ce8273782c05897764d2119a58a2e28
lincolnmay01 <ljmay002@louisville.edu>
2024-12-28 23:47:44 -0500

    hamburger updates

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ document.getElementById('emailButton').addEventListener('click', function() {
     window.location.href = 'mailto:kristinakf04@gmail.com';
   });
 
-  document.getElementById("linkedinbutton").addEventListener("click", function() {
-    var linkedInUrl = "https://www.linkedin.com/in/lincoln-may";   
-    window.open(linkedInUrl, "_blank");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active"); 
+      navLinks.classList.toggle("active");
+  });
+
+  // Optional: Close the menu when a link is clicked
+  document.querySelectorAll('.nav-links li').forEach(item => 
+    item.addEventListener('click', () => {
+      navLinks.classList.remove('active');
+      hamburger.classList.remove('active');
+    }));
 });
+
+window.onload = function() {
+  // Reset the form fields when the page loads
+  document.getElementById("contact-form").reset();
+};
